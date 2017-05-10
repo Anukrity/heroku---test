6506a12b7fadf59507ebf8ce110fe6ab8ea97b70
Anukrity <anukrity.in@gmail.com>
2017-05-10 11:08:39 +0530

    change in app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express=require('express');
 var app=express();
-
+var port=5000;
 app.use('/',function(req,res,next){
 res.send('hello world by me');
 });
 
-app.listen(process.env.PORT || 5000,function(){
+app.listen(process.env.PORT || port,function(){
 console.log('app listening on server port',port)
 });
